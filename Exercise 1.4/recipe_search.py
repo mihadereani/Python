5974ea40c509ac35c7d2826b03a9e9332a98e88a
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-04-18 16:26:39 +0100

    Define a function to display a recipe

--- Exercise 1.4/recipe_search.py
+++ Exercise 1.4/recipe_search.py
@@ -1,1 +1,10 @@
 import pickle
+
+
+def display_recipe(recipe):
+    print('''Recipe details
+    =====================''')
+    print('Name: ', recipe['name'])
+    print('Cooking time (min): ', recipe['cooking_time'])
+    print('Ingredients: ', recipe['ingredients'])
+    print('Difficulty: ', recipe['difficluty'])
