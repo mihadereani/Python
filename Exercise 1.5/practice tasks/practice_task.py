b70776ceb5605f69fe76aa3e99d7d0245884627a
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-04-22 13:00:35 +0100

    add practice task 3

--- Exercise 1.5/practice tasks/practice_task.py
+++ Exercise 1.5/practice tasks/practice_task.py
@@ -36,6 +36,36 @@ class Height(object):
 
         return Height(output_feet, output_inches)
 
+    def __lt__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A < height_inches_B
+
+    def __le__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A <= height_inches_B
+
+    def __eq__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A == height_inches_B
+
+    def __gt__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A > height_inches_B
+
+    def __ge__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A >= height_inches_B
+
+    def __ne__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A != height_inches_B
+
 
 person_A_height = Height(5, 10)
 person_B_height = Height(4, 10)
@@ -44,3 +74,10 @@ height_sub = person_A_height - person_B_height
 
 print("Added height:", height_sum)
 print("Subtracted height:", height_sub)
+
+print(Height(4, 5) < Height(4, 6))
+print(Height(4, 5) <= Height(4, 5))
+print(Height(5, 10) == Height(5, 10))
+print(Height(4, 6) > Height(4, 5))
+print(Height(4, 5) >= Height(4, 5))
+print(Height(5, 9) != Height(5, 10))
