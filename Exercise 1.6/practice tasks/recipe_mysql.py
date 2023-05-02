c6a60eea0d6025e78e3fde903dc31dfe9e70989d
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-05-02 11:32:49 +0100

    Create & Connect Database

---
+++ Exercise 1.6/practice tasks/recipe_mysql.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='cf-python',
+    passwd='password')
+cursor = conn.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS task_database")
+cursor.execute("USE task_database")
+cursor.execute('''CREATE TABLE IF NOT EXISTS Recipes(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(50),
+    ingredients     VARCHAR(255),
+    cooking_time    INT,
+    difficulty      VARCHAR(20))
+    ''')
