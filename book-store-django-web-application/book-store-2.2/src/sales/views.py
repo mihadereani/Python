a23afeb5abe4a21dd338d6ab87fd670ee3f8beec
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-06-02 07:59:22 +0200

    add a User-Input Form

--- book-store-django-web-application/book-store-2.2/src/sales/views.py
+++ book-store-django-web-application/book-store-2.2/src/sales/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # to protect function-based views
 from django.contrib.auth.decorators import login_required
+from .forms import SalesSearchForm
 
 
 # Create your views here.
@@ -12,9 +13,15 @@ def home(request):
 # define function-based view - records(records()
 
 
-# define function-based view - records(request)
-# keep protected
-@login_required
+# define function-based view - records()
 def records(request):
-    # do nothing, simply display page
-    return render(request, 'sales/records.html')
+    # create an instance of SalesSearchForm that you defined in sales/forms.py
+    form = SalesSearchForm(request.POST or None)
+
+    # pack up data to be sent to template in the context dictionary
+    context = {
+        'form': form,
+    }
+
+    # load the sales/record.html page using the data that you just prepared
+    return render(request, 'sales/records.html', context)
