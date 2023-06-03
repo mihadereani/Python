45303efab2c5014464785013c12c8aaeefdc68dd
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-06-03 17:11:07 +0200

    istall pandas

--- book-store-django-web-application/book-store-2.2/src/sales/views.py
+++ book-store-django-web-application/book-store-2.2/src/sales/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Sale
 # to protect function-based views
 from django.contrib.auth.decorators import login_required
 from .forms import SalesSearchForm
+from .models import Sale
 
 
 # Create your views here.
@@ -13,10 +15,36 @@ def home(request):
 # define function-based view - records(records()
 
 
-# define function-based view - records()
 def records(request):
     # create an instance of SalesSearchForm that you defined in sales/forms.py
     form = SalesSearchForm(request.POST or None)
+
+    # check if the button is clicked
+    if request.method == 'POST':
+        # read book_title and chart_type
+        book_title = request.POST.get('book_title')
+        chart_type = request.POST.get('chart_type')
+        # display in terminal - needed for debugging during development only
+        print(book_title, chart_type)
+
+        print('Exploring querysets:')
+        print('Case 1: Output of Sale.objects.all()')
+        qs = Sale.objects.all()
+        print(qs)
+
+        print('Case 2: Output of Sale.objects.filter(book_name=book_title)')
+        qs = Sale.objects.filter(book__name=book_title)
+        print(qs)
+
+        print('Case 3: Output of qs.values')
+        print(qs.values())
+
+        print('Case 4: Output of qs.values_list()')
+        print(qs.values_list())
+
+        print('Case 5: Output of Sale.objects.get(id=1)')
+        obj = Sale.objects.get(id=1)
+        print(obj)
 
     # pack up data to be sent to template in the context dictionary
     context = {
