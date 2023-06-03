ea83fb9d10c727555aa94879bd37e75bf2fb33f0
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-06-03 19:39:53 +0200

    Visualization and Charting

--- book-store-django-web-application/book-store-2.2/src/sales/views.py
+++ book-store-django-web-application/book-store-2.2/src/sales/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from .models import Sale
-# to protect function-based views
-from django.contrib.auth.decorators import login_required
 from .forms import SalesSearchForm
 from .models import Sale
 import pandas as pd
-from .utils import get_bookname_from_id
-
+from .utils import get_bookname_from_id, get_chart
 
 # Create your views here.
 
@@ -14,14 +10,14 @@ from .utils import get_bookname_from_id
 def home(request):
     return render(request, 'sales/home.html')
 
-# define function-based view - records(records()
+# define function-based view - records()
 
 
 def records(request):
     # create an instance of SalesSearchForm that you defined in sales/forms.py
     form = SalesSearchForm(request.POST or None)
     sales_df = None  # initialize dataframe to None
-
+    chart = None  # initialize chart to None
     # check if the button is clicked
     if request.method == 'POST':
         # read book_title and chart_type
@@ -36,12 +32,44 @@ def records(request):
             # convert the ID to Name of book
             sales_df['book_id'] = sales_df['book_id'].apply(
                 get_bookname_from_id)
-            # convert the dataframe to HTML
+
+            # call get_chart by passing chart_type from user input, sales dataframe and labels
+            chart = get_chart(chart_type, sales_df,
+                              labels=sales_df['date_created'].values)
+
+           # convert the dataframe to HTML
             sales_df = sales_df.to_html()
+
+        '''
+       The following block is to get introduced to querysets
+       #display in terminal - needed for debugging during development only
+       print (book_title, chart_type)
+
+       print ('Exploring querysets:')
+       print ('Output of Sale.objects.all()')
+       qs=Sale.objects.all()
+       print (qs)
+
+       print ('Output of Sale.objects.filter(book__name=book_title)')
+       qs =Sale.objects.filter(book__name=book_title)
+       print (qs)
+
+       print ('Output of qs.values()')
+       print (qs.values())
+
+       print ('Output of qs.values_list()')
+       print (qs.values_list())
+
+       print ('Output of Sale.objects.get(id=1)')
+       obj = Sale.objects.get(id=1)
+       print (obj)
+       '''
+
     # pack up data to be sent to template in the context dictionary
     context = {
         'form': form,
         'sales_df': sales_df,
+        'chart': chart
     }
 
     # load the sales/record.html page using the data that you just prepared
